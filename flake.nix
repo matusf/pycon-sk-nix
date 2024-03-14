9751e1c5a6dcbd163d064838b3608909cd5de7c5
Matúš Ferech <matus.ferech@gmail.com>
2024-03-14 21:05:53 +0100

    Build docker image

--- flake.nix
+++ flake.nix
@@ -21,9 +21,17 @@
       pkgs = nixpkgs.legacyPackages.${system};
       inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication;
     in {
-      packages = {
+      packages = rec {
         myapp = mkPoetryApplication {projectDir = self;};
         default = self.packages.${system}.myapp;
+        docker = pkgs.dockerTools.buildImage {
+          name = "app";
+          tag = "0.1.0";
+          created = "now";
+          config = {
+            Cmd = ["${myapp}/bin/app"];
+          };
+        };
       };
 
       devShells.default = pkgs.mkShell {
